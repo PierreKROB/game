18a51c6dff950e7c3832cbf966915bc371c7e258
PierreKROB <krobpierre0@gmail.com>
2023-07-31 16:00:37 +0200

    select team

--- battle/battle.php
+++ battle/battle.php
@@ -14,19 +14,13 @@ session_start();
         if (isset($_POST['selected_characters']) && !empty($_POST['selected_characters'])) {
             // Ajouter une ligne de débogage pour afficher les données reçues
             echo "<pre>";
-            var_dump($_POST['selected_characters[1]']);
+            var_dump($_POST['selected_characters'][1]);
             echo "</pre>";
-
             // Obtenez les personnages sélectionnés et affichez-les sous forme de tableau
             echo "HP: $HP";
             foreach ($_POST['selected_characters'] as $selected_character_id) {
                 // Vous pouvez également utiliser l'API pour obtenir plus d'informations sur chaque personnage ici
-                echo "<tr>
-                        <td>$selected_character_id</td>
-                        <td>Personnage $selected_character_id</td>
-                      </tr>";
             }
-            echo "</table>";
         } else {
             echo "Aucun personnage sélectionné. Veuillez retourner à la page précédente et en sélectionner au moins un.";
         }
