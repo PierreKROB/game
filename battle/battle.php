a5dc8498dfe4f978cd4f8b07fa5249644d585f77
PierreKROB <krobpierre0@gmail.com>
2023-07-31 16:13:25 +0200

    select team

--- battle/battle.php
+++ battle/battle.php
@@ -1,29 +1,37 @@
-<?php 
+<?php
 session_start();
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Équipe de combat</title>
 </head>
+
 <body>
     <h1>Équipe de combat</h1>
-    <?php      
-        // Vérifiez si des personnages ont été sélectionnés
-        if (isset($_POST['selected_characters']) && !empty($_POST['selected_characters'])) {
-            // Ajouter une ligne de débogage pour afficher les données reçues
-            echo "<pre>";
-            var_dump($_POST['selected_characters'][1]);
-            echo "</pre>";
-            // Obtenez les personnages sélectionnés et affichez-les sous forme de tableau
-            echo "HP: $HP";
-            foreach ($_POST['selected_characters'] as $selected_character_id) {
-                // Vous pouvez également utiliser l'API pour obtenir plus d'informations sur chaque personnage ici
-            }
-        } else {
-            echo "Aucun personnage sélectionné. Veuillez retourner à la page précédente et en sélectionner au moins un.";
+    <?php
+    // Vérifiez si des personnages ont été sélectionnés
+    if (isset($_POST['selected_characters']) && !empty($_POST['selected_characters'])) {
+        $user_id = $_SESSION['user_id'];
+        $api_url = "https://eligoal.com/game/api/player_characters/$user_id";
+
+        // Utilisation de file_get_contents pour accéder à l'API
+        $characters_json = file_get_contents($api_url);
+        $characterDetails = json_decode($characters_json, true);
+
+        echo $characterDetails[1];
+
+
+        echo "HP: $HP";
+        foreach ($_POST['selected_characters'] as $selected_character_id) {
+            // Vous pouvez également utiliser l'API pour obtenir plus d'informations sur chaque personnage ici
         }
+    } else {
+        echo "Aucun personnage sélectionné. Veuillez retourner à la page précédente et en sélectionner au moins un.";
+    }
     ?>
 </body>
+
 </html>
