857572e4f90cb183c7e9d23fe8dd99d70261ddfe
PierreKROB <krobpierre0@gmail.com>
2023-07-31 14:04:15 +0200

    correctif fonction gettallcharacter

--- box.php
+++ box.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
 if (isset($_SESSION['username'])) {
-    
+    $user_id = $_SESSION['user_id'];
+    $api_url = "./api/characters/$user_id";
+
+    // Utilisation de file_get_contents pour accéder à l'API
+    $characters_json = file_get_contents($api_url);
+    $characterDetails = json_decode($characters_json, true);
+
+    if ($characterDetails !== null) {
 ?>
 
 <!DOCTYPE html>
@@ -19,37 +26,20 @@ if (isset($_SESSION['username'])) {
         <h1>Liste des personnages</h1>
         <ul>
             <?php
-            $user_id = $_SESSION['user_id'];
-            $api_url = "http://votre-api.com/api.php?demande=personnages/$user_id";
-
-            // Utilisation de cURL pour effectuer la requête à l'API
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, $api_url);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($curl);
-            $http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-            curl_close($curl);
-
-            if ($http_code == 200) {
-                $characterDetails = json_decode($response, true);
-
-                // Boucle pour afficher les informations de chaque personnage
-                foreach ($characterDetails as $character) {
-                    $hp_affiche = $character['hp'] * pow((1 + ($character['lvl'] / 100)), 2);
-                    $puissance_affiche = $character['power'] * pow((1 + ($character['lvl'] / 100)), 2);
-                    $def_affiche = $character['defense'] * pow((1 + ($character['lvl'] / 100)), 2);
+            // Boucle pour afficher les informations de chaque personnage
+            foreach ($characterDetails as $character) {
+                $hp_affiche = $character['hp'] * pow((1 + ($character['lvl'] / 100)), 2);
+                $puissance_affiche = $character['power'] * pow((1 + ($character['lvl'] / 100)), 2);
+                $def_affiche = $character['defense'] * pow((1 + ($character['lvl'] / 100)), 2);
             ?>
-                    <li>
-                        <?php echo htmlspecialchars($character['name']); ?><br>
-                        Puissance: <?php echo number_format($puissance_affiche, 2); ?><br>
-                        Défense: <?php echo number_format($def_affiche, 2); ?><br>
-                        HP: <?php echo number_format($hp_affiche, 2); ?><br>
-                        Niveau: <?php echo $character['lvl']; ?><br>
-                    </li>
+                <li>
+                    <?php echo htmlspecialchars($character['name']); ?><br>
+                    Puissance: <?php echo number_format($puissance_affiche, 2); ?><br>
+                    Défense: <?php echo number_format($def_affiche, 2); ?><br>
+                    HP: <?php echo number_format($hp_affiche, 2); ?><br>
+                    Niveau: <?php echo $character['lvl']; ?><br>
+                </li>
             <?php
-                }
-            } else {
-                echo "Erreur lors de la récupération des données depuis l'API.";
             }
             ?>
         </ul>
@@ -60,6 +50,9 @@ if (isset($_SESSION['username'])) {
 </html>
 
 <?php
+    } else {
+        echo "Erreur lors de la récupération des données depuis l'API.";
+    }
 } else {
     header("Location: index.php");
     exit;
