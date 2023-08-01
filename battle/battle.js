70e18f2bf08b549e1395420a52cc271cda3fecdd
PierreKROB <krobpierre0@gmail.com>
2023-08-01 16:31:53 +0200

    modif api

--- battle/battle.js
+++ battle/battle.js
@@ -1,12 +1,12 @@
 // Classe HP_TOTAL
-class HP_TOTAL {
+class HP_TOTALplayer {
     constructor(HP_MAX) {
       this.HP_MAX = HP_MAX;
       this.HP_NOW = HP_MAX;
     }
   
     // Méthode pour ajouter des points de vie
-    Heal(montant) {
+    Healplayer(montant) {
       this.HP_NOW += montant;
       // Vérifier si les points de vie actuels ne dépassent pas le maximum
       if (this.HP_NOW > this.HP_MAX) {
@@ -15,7 +15,7 @@ class HP_TOTAL {
     }
   
     // Méthode pour retirer des points de vie
-    Damage(montant) {
+    Damageplayer(montant) {
       this.HP_NOW -= montant; // Correction ici, utiliser this.HP_NOW au lieu de pointsDeVieActuels
       // Vérifier si les points de vie actuels sont tombés en dessous de zéro
       if (this.HP_NOW < 0) {
