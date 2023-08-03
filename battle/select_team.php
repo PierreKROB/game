9256c9f308e0fe33e6a3c3bf290b33ecde1fc9ca
PierreKROB <krobpierre0@gmail.com>
2023-08-03 21:22:13 +0200

    encore des modif battle.js and co

--- battle/select_team.php
+++ battle/select_team.php
@@ -44,7 +44,7 @@ $user_id = $_SESSION['user_id'];
 
                     // Affichage des statistiques du personnage avec une case à cocher
                     echo "<label>";
-                    echo "<input type=\"checkbox\" name=\"selected_characters[]\" value=\"$character\">";
+                    echo "<input type=\"checkbox\" name=\"selected_characters[]\" 'value='".htmlspecialchars(json_encode($character))."'>";
                     echo "$character_name - HP: $character_hp";
                     echo "</label><br>";
                 }
