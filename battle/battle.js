f9d53ddc1a70e4561be714759b8fb11957c6e4b9
PierreKROB <krobpierre0@gmail.com>
2023-08-09 17:11:13 +0200

    rotation try 3

--- battle/battle.js
+++ battle/battle.js
@@ -208,6 +208,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const listeperso = new CombatListe(personnages)
   console.log(listeperso.liste)
-  listeperso.nextRotation()
-  console.log(listeperso.liste)
+  const feur = listeperso.nextRotation()
+  console.log(feur)
 });
