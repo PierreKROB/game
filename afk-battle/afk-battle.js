64ac9ee0e276ff7def6900b5c8fb43195b59dd33
PierreKROB <krobpierre0@gmail.com>
2023-08-16 11:48:23 +0200

    ajout afkbattle

--- afk-battle/afk-battle.js
+++ afk-battle/afk-battle.js
@@ -4,22 +4,43 @@ var c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Create empty array
-var images = [];
-images.length = 10;
+// Créer des tableaux d'images pour les deux types d'animations
+var staticImages = [];
+var kamehamehaImages = [];
 
-// Push the images into array
-for (var i = 0; i < images.length; i++) {
-    images[i] = new Image();
-    images[i].src = './animations/Goku/static/goku-static' + (i + 1) + '.png';
+// Remplir les tableaux d'images pour les deux types d'animations
+for (var i = 0; i < 10; i++) {
+    staticImages[i] = new Image();
+    staticImages[i].src = './animations/Goku/static/goku-static' + (i + 1) + '.png';
+
+    kamehamehaImages[i] = new Image();
+    kamehamehaImages[i].src = './animations/Goku/super-attack/goku-kamehameha' + (i + 1) + '.png';
 }
 
-var i = 0; // Start from the first image
+var frameIndex = 0; // Index de la frame actuelle
+var isKamehameha = false; // Indicateur pour l'animation du Kamehameha
+
+// Interval pour l'animation générale (toutes les 4 secondes)
 setInterval(function () {
-    c.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-    c.drawImage(images[i], 100, 100, 100, 100);
-    i++;
-    if (i >= images.length) {
-        i = 0;
+    isKamehameha = !isKamehameha; // Alterner entre les deux animations
+    frameIndex = 0; // Réinitialiser l'index de frame
+
+    // Effacer le canvas
+    c.clearRect(0, 0, canvas.width, canvas.height);
+}, 4000);
+
+// Interval pour afficher les frames individuelles
+setInterval(function () {
+    c.clearRect(0, 0, canvas.width, canvas.height); // Effacer le canvas
+
+    // Choisir le tableau d'images en fonction de l'animation actuelle
+    var currentImages = isKamehameha ? kamehamehaImages : staticImages;
+
+    // Dessiner l'image courante
+    c.drawImage(currentImages[frameIndex], 100, 100, 100, 100);
+
+    frameIndex++;
+    if (frameIndex >= 10) {
+        frameIndex = 0;
     }
 }, 100);
